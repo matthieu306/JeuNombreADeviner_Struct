a95121220918a8d7bf5b12a0fbe2014ec7d4d0b8
matthieu306 <75265527+matthieu306@users.noreply.github.com>
2021-02-07 21:52:41 +0100

    Update MainJeuNombreADeviner.cpp

--- src/MainJeuNombreADeviner.cpp
+++ src/MainJeuNombreADeviner.cpp
@@ -1,10 +1,10 @@
 /*************************************************/
-// Nom du projet: Jeu du nombre à deviner
+// Nom du projet: Jeu du nombre Ã  deviner
 // Nom du fichier: MainJeuNombreAdeviner.cpp
 // Version : 1.0
 // Nom du programmeur: MP Pinaud
-// Date de création : 25/01/2021
-// Rôle du fichier: Contient le code jeu
+// Date de crÃ©ation : 25/01/2021
+// RÃ´le du fichier: Contient le code jeu
 // Nom des composants utilises: JeuNombreADeviner.h
 //                              iostream
 // Historique du fichier:
@@ -16,7 +16,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Vous allez jouer pour deviner un nombre secret" << endl;
-
+    string un_nom;
+    TJoueur joueurAcreer;
+    cout << "Quel est votre nom ?" << endl;
+    cin >> un_nom;
+    cout << "Quel est le nombre mistere ? il est entre 1 & 10 !" << endl;
+    InitJoueur(joueurAcreer, un_nom);
+    JouerPartie(joueurAcreer, TirerNombreMystere());
+    MajResultatsJoueur(joueurAcreer, joueurAcreer.nbTentatives, joueurAcreer.nbPartiesGagnees);
+    ResultatsJoueur(joueurAcreer, joueurAcreer.nbPartiesGagnees, joueurAcreer.nbPartiesPerdues, joueurAcreer.nbTentatives);
+    cout << "Tu as gagne en " << joueurAcreer.nbTentatives << " tentatives, tu as reussi " << joueurAcreer.nbPartiesGagnees << " parties, et tu as perdu " << joueurAcreer.nbPartiesPerdues << " parties.";
     return 0;
 }
